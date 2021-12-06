39b7ddf26909b021f8c4c1b15d2eaf790f332f1f
Jeff Jacob <phansoft@gmail.com>
2021-12-05 23:12:10 -0800

    Day 1 part 1

--- Advent2021/Program.cs
+++ Advent2021/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string inputFileName = @"App_Data\input.txt";
+
+var inputFile = File.OpenText(inputFileName);
+
+var line = inputFile.ReadLine();
+var increases = 0;
+long currentValue = long.MaxValue;
+try
+{
+    while (line != null)
+    {
+        var lastValue = currentValue;
+        currentValue = long.Parse(line);
+        if (currentValue > lastValue)
+        {
+            increases++;
+        }
+        //Console.WriteLine(line);
+        line = inputFile.ReadLine();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+Console.WriteLine(increases);
